be171727c3fad8dd5e15181ad7ba4e7feb8da759
MotheLavanya <mothelavanya311@gmail.com>
2025-06-01 14:53:15 +0530

    file added

---
+++ differentoperators.py
@@ -0,0 +1,56 @@
+# DIFFERENT TYPES OF OPERATORS:
+# arthematic operators:
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+#Assignment operators:
+
+a += b
+print(a)
+
+a-= b
+print(a)
+
+a*= b
+print(a)
+
+a/=b
+print(a)
+
+a%=b
+print(a)
+
+a//=b
+print(a)
+
+a**=b
+print(a)
+
+#Comparsion Operator:
+print(a<b)
+print(a>b)
+print(a>=b)
+print(a<=b)
+print(a!=0)
+print(a==b)
+
+# Logical Operator:
+a = True
+b = False
+print(a and b)
+print( a or b)
+print(not b)
+print(not a )
+
+#Bitwise Operators:
+a = 5
+b = -10
+print("a << 1 =", a << 1)
+print("b >> 1 =", b >> 1)
